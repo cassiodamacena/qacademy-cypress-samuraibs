ffe5e88d28f4a5cab7d554daf4454b6b9d5ef394
Cassio Damacena <cassiodamacena@hotmail.com>
2022-06-28 22:47:29 -0300

    Configurações da conexeção de banco de dados no arquivo cypress.json

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,13 +22,8 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  const pool = new Pool({
-    host: 'ziggy.db.elephantsql.com',
-    user: 'zjfhlpai',
-    password: 'xK3BgkIz0Vwt3ZznyADD2TmNzKBzGfew',
-    database: 'zjfhlpai',
-    port: 5432
-  })
+  const configJson = require(config.configFile)
+  const pool = new Pool(configJson.dbConfig)
 
   on('task', {
     removeUser(email) {

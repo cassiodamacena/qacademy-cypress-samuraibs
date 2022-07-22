52bc8e9cf26a1115c1104dd0a30605832f39a35a
Cassio Damacena <cassiodamacena@hotmail.com>
2022-07-22 13:48:51 -0300

    Implementando massa de testes com Factories

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -2,36 +2,21 @@
 
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
+import {client, provider, appointment} from '../support/factories/dash'
 
 describe('Dado que desejo validar o dashboard', function () {
 
     context('Quando o cliente faz um agendamento no app mobile', function () {
 
-        const data = {
-            client: {
-                name: 'Nikki Sixx',
-                email: 'sixx@motleycrue.com',
-                password: 'pwd123',
-                is_provider: false
-            },
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'ramo@samuraibs.com',
-                password: 'pwd123',
-                is_provider: true
-            },
-            appointmentHour: '14:00'
-        }
-
         before(function () {
-            cy.postUser(data.provider)
-            cy.postUser(data.client)
+            cy.postUser(provider)
+            cy.postUser(client)
 
-            cy.loginApp(data.client)
+            cy.loginApp(client)
             cy.log('Token :' + Cypress.env('loginAppClient'))
 
-            cy.setProviderId(data.provider.email)
-            cy.createAppointment(data.appointmentHour)
+            cy.setProviderId(provider.email)
+            cy.createAppointment(appointment.hour)
         })
 
         it('Então o mesmo deve ser exibido no dashboard', function () {
@@ -40,12 +25,12 @@ describe('Dado que desejo validar o dashboard', function () {
             cy.log('Id Ramom: ' + Cypress.env('providerId'))
 
             loginPage.go()
-            loginPage.form(data.provider)
+            loginPage.form(provider)
             loginPage.submit()
 
             dashPage.calendarShouldBeVisible()
             dashPage.selectDay(Cypress.env('appointmentDay'))
-            dashPage.appointmentShouldBeVisible(data.client, data.appointmentHour)
+            dashPage.appointmentShouldBeVisible(client, appointment.hour)
 
         })
 
